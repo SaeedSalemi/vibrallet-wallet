7d68c68ecb68f89357f3de846dcff1096fb745f9
danial <dev.salahi@gmail.com>
2021-10-21 00:41:36 +0300

    added files for metro,shim

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,4 @@
-// // Learn more https://docs.expo.io/guides/customizing-metro
-// const { getDefaultConfig } = require('expo/metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-// module.exports = getDefaultConfig(__dirname);
-
-const extraNodeModules = require('node-libs-browser');
-module.exports = {
-  resolver: {
-    extraNodeModules,
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-    transform: {
-      experimentalImportSupport: false,
-      inlineRequires: false,
-    },
-   }),
-  },
-};
+module.exports = getDefaultConfig(__dirname);
