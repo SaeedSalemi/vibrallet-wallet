413db484e0963af4e74f87f4db517cc70d1d776a
danial <dev.salahi@gmail.com>
2021-10-24 13:31:10 +0300

    Completed the AppLoader Component for global using in the APP

---
+++ src/components/common/AppLoader/AppLoader.js
@@ -0,0 +1,26 @@
+import { ActivityIndicator, View } from 'react-native'
+import { globalStyles } from '../../../config/styles'
+
+const AppLoader = ({ size = 80, color = globalStyles.Colors.primaryColor }) => {
+  return (
+    <View
+      style={{
+        ...globalStyles.flex.center,
+        flex: 1,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        backgroundColor: globalStyles.Colors.inputColorOpacity,
+      }}
+    >
+      <ActivityIndicator
+        size={size}
+        color={color}
+      />
+    </View>
+  )
+}
+
+export default AppLoader
