126cd343e2c6e48f0ba5014b32aebe8da1f9eef6
danial <dev.salahi@gmail.com>
2021-10-25 11:04:22 +0300

    update the App.js

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 import SplashScreen from './src/screens/SplashScreen'
 import { Keyboard, TouchableWithoutFeedback } from 'react-native'
 import HideKeyboard from './src/utils/HideKeyboard'
+import InAppNotificaiton from './src/components/common/AppNotification/InAppNotification'
 
 const { store, persistor } = configStore()
 
@@ -19,6 +20,7 @@ export default function App() {
 				<SafeAreaProvider
 					style={{ backgroundColor: globalStyles.Colors.bckColor }}
 				>
+					<InAppNotificaiton />
 					<StatusBar backgroundColor={globalStyles.Colors.bckColor} barStyle="light-content" />
 					<HideKeyboard>
 						<RootNavigation />
