4911b5e26148594347a018312693867a199f88eb
danial <dev.salahi@gmail.com>
2021-10-28 18:30:47 +0300

    refactor notifications

--- src/screens/app/HomeStack/NotificationScreen.js
+++ src/screens/app/HomeStack/NotificationScreen.js
@@ -6,6 +6,7 @@ import ProfileMenu from '../../../components/Profile/ProfileMenu'
 import { globalStyles } from '../../../config/styles'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import AppButton from '../../../components/common/AppButton'
+import AppText from '../../../components/common/AppText'
 
 const withdrawal = 'Withdrawal Successful'
 const deposit = 'Deposit Unsuccessful'
@@ -13,114 +14,126 @@ const detail =
 	'You have successfully withdrawn 234.5 Usdt at 2021-02-29 18:21:14 (UTC).'
 const time = '18:21:14'
 export default function NotificationScreen() {
+	// const items = useMemo(
+	// 	() => [
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text1}
+	// 				/>
+	// 			),
+	// 			title: withdrawal,
+	// 			detail: detail,
+	// 			time: time,
+	// 		},
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text3}
+	// 				/>
+	// 			),
+	// 			title: deposit,
+	// 			detail: detail,
+	// 			time: time,
+	// 			read: true,
+	// 		},
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text1}
+	// 				/>
+	// 			),
+	// 			title: withdrawal,
+	// 			detail: detail,
+	// 			time: time,
+	// 		},
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text3}
+	// 				/>
+	// 			),
+	// 			title: deposit,
+	// 			detail: detail,
+	// 			time: time,
+	// 			read: true,
+	// 		},
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text1}
+	// 				/>
+	// 			),
+	// 			title: deposit,
+	// 			detail: detail,
+	// 			time: time,
+	// 		},
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text3}
+	// 				/>
+	// 			),
+	// 			title: withdrawal,
+	// 			detail: detail,
+	// 			time: time,
+	// 			read: true,
+	// 		},
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text1}
+	// 				/>
+	// 			),
+	// 			title: deposit,
+	// 			detail: detail,
+	// 			time: time,
+	// 		},
+	// 		{
+	// 			icon: (
+	// 				<Ionicons
+	// 					name="notifications-outline"
+	// 					size={25}
+	// 					color={globalStyles.Colors.text1}
+	// 				/>
+	// 			),
+	// 			title: deposit,
+	// 			detail: detail,
+	// 			time: time,
+	// 		},
+	// 	],
+	// 	[]
+	// )
+
 	const items = useMemo(
-		() => [
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text1}
-					/>
-				),
-				title: withdrawal,
-				detail: detail,
-				time: time,
-			},
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text3}
-					/>
-				),
-				title: deposit,
-				detail: detail,
-				time: time,
-				read: true,
-			},
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text1}
-					/>
-				),
-				title: withdrawal,
-				detail: detail,
-				time: time,
-			},
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text3}
-					/>
-				),
-				title: deposit,
-				detail: detail,
-				time: time,
-				read: true,
-			},
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text1}
-					/>
-				),
-				title: deposit,
-				detail: detail,
-				time: time,
-			},
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text3}
-					/>
-				),
-				title: withdrawal,
-				detail: detail,
-				time: time,
-				read: true,
-			},
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text1}
-					/>
-				),
-				title: deposit,
-				detail: detail,
-				time: time,
-			},
-			{
-				icon: (
-					<Ionicons
-						name="notifications-outline"
-						size={25}
-						color={globalStyles.Colors.text1}
-					/>
-				),
-				title: deposit,
-				detail: detail,
-				time: time,
-			},
-		],
+		() => [],
 		[]
 	)
 	return (
 		<View style={{ ...globalStyles.gapScreen }}>
-			<NotificationItem data={items} />
-			<AppButton
+			{items.length > 0 && <NotificationItem data={items} />}
+
+			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				<AppText style={{ color: globalStyles.Colors.inputColor }}>
+					You don't have notifications.
+				</AppText>
+			</View>
+
+			{items.length > 0 && <AppButton
 				textStyle={{ color: globalStyles.Colors.primaryColor }}
 				title="Mark all as Read"
 				customStyle={{
@@ -128,7 +141,8 @@ export default function NotificationScreen() {
 					borderWidth: 1,
 					borderColor: globalStyles.Colors.inputColor,
 				}}
-			/>
+			/>}
+
 		</View>
 	)
 }
