4453d6682f2607403777491ec220e92951782feb
danial <dev.salahi@gmail.com>
2021-11-04 20:13:32 +0300

    dynamically of the items in fav list

--- src/screens/app/MarketStack/EditFavorites.js
+++ src/screens/app/MarketStack/EditFavorites.js
@@ -1,12 +1,29 @@
-import React, { useMemo } from 'react'
-import { FlatList, View } from 'react-native'
+import React, { useMemo, useState, useContext } from 'react'
+// import { FlatList, View } from 'react-native'
 import { globalStyles } from '../../../config/styles'
 import AppText from '../../../components/common/AppText'
 import FavoritesItems from '../../../components/Market/FavoritesItems'
 import HR from '../../../components/common/HR/HR'
 import { ScrollView } from 'react-native-gesture-handler'
+import DraggableFlatList from 'react-native-draggable-flatlist'
+import {
+	View,
+	Platform
+} from 'react-native';
+
+import { Context } from '../../../context/marketContext'
+
+const Separator = () => {
+	return <View style={Platform.OS === "android" ? styles.separator : null} />;
+}
 
 export default function EditFavorites() {
+
+	const { coins } = useContext(Context)
+	const filteredFavCoins = coins.filter(coin => coin.fav === true)
+
+
+
 	const items = useMemo(() => {
 		return [
 			'BTC/USDT',
@@ -17,6 +34,22 @@ export default function EditFavorites() {
 			'BTC/USDT',
 		]
 	}, [])
+
+
+	const renderCoinItem = ({ item, index, drag, isActive }) => {
+		return (
+			<View key={index}>
+				<View style={{ paddingHorizontal: 18, marginVertical: 18 }}>
+					<FavoritesItems title={item} onDrag={drag} />
+				</View>
+				{index + 1 !== items.length ? (
+					<HR style={{ marginVertical: 16 }} />
+				) : null}
+			</View>
+		)
+	}
+
+
 	return (
 		<View style={{ ...globalStyles.screen }}>
 			<View
@@ -35,19 +68,10 @@ export default function EditFavorites() {
 				<AppText color="text2">Sort</AppText>
 			</View>
 
-			<FlatList
+			<DraggableFlatList
 				data={items}
 				style={{ marginVertical: 18 }}
-				renderItem={({ item, index }) => (
-					<View key={index}>
-						<View style={{ paddingHorizontal: 18, marginVertical: 18 }}>
-							<FavoritesItems title={item} />
-						</View>
-						{index + 1 !== items.length ? (
-							<HR style={{ marginVertical: 16 }} />
-						) : null}
-					</View>
-				)}
+				renderItem={renderCoinItem}
 				keyExtractor={(_, index) => index.toString()}
 			/>
 		</View>
