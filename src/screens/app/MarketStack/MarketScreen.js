010f286f7f9f7e069e170b08fb593fea678919cd
danial <dev.salahi@gmail.com>
2021-11-04 20:01:34 +0300

    refactor market screen

--- src/screens/app/MarketStack/MarketScreen.js
+++ src/screens/app/MarketStack/MarketScreen.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { View } from 'react-native'
 import MarketHeader from '../../../components/Market/MarketHeader'
 import Screen from '../../../components/Screen'
-import MarketProvider from '../../../context/marketContext'
 import { FCASRatingTabNavigator } from '../../../navigation/FCASRatingStack'
 import { MarketTabNavigator } from '../../../navigation/MarketTabStack'
 
@@ -11,10 +10,8 @@ export default function MarketScreen() {
 
 	return (
 		<Screen gap={false}>
-			<MarketProvider>
-				<MarketHeader isMarket={isMarket} setIsMarket={setIsMarket} />
-				{isMarket ? <MarketTabNavigator /> : <FCASRatingTabNavigator />}
-			</MarketProvider>
+			<MarketHeader isMarket={isMarket} setIsMarket={setIsMarket} />
+			{isMarket ? <MarketTabNavigator /> : <FCASRatingTabNavigator />}
 		</Screen>
 	)
 }
