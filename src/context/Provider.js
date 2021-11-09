cbcdc72304f5c6b2eb438836297411b9485f2158
danial <dev.salahi@gmail.com>
2021-11-09 18:03:58 +0300

    added FCAS rating hooks into the global context

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,20 +1,37 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 import useCoins from '../hooks/useCoins'
+import useFCASRating from '../hooks/useFCASRating'
+// import { } from './'
 
 export const Context = createContext()
 
 const MainProvider = props => {
   const [state, setState] = useState({
+    user: {},
+    wallet: {}
   })
+  const [render, setRender] = useState(false)
 
 
   const { coins, setCoin } = useCoins()
+  const { list: FCASList } = useFCASRating()
 
   const dispatch = value => setState({ ...state, ...value })
 
 
+
+
+
+  useEffect(() => {
+
+  }, [])
+
+
+
+
+
   return (
-    <Context.Provider value={{ ...state, coins, setCoin, dispatch }}>
+    <Context.Provider value={{ ...state, coins, FCASList, setCoin, dispatch }}>
       {props.children}
     </Context.Provider>
   )
