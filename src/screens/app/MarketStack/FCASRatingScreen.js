d5a93298ba5d3af10d674aa460c68619743de2db
danial <dev.salahi@gmail.com>
2021-11-10 13:47:48 +0300

    render FCAS listing to the ui

--- src/screens/app/MarketStack/FCASRatingScreen.js
+++ src/screens/app/MarketStack/FCASRatingScreen.js
@@ -11,83 +11,83 @@ import { Context } from '../../../context/Provider'
 export default function FCASRatingScreen() {
 
 	const { FCASList } = useContext(Context)
-
-	const colors = [
-		'#67B010',
-		'#4ED69D',
-		'#87C0A9',
-		'#F69B4F',
-		'#F84837',
-		'#87C0A9',
-	]
+	const colors = {
+		'S': '#67B010',
+		'A': '#4ED69D',
+		'B': '#87C0A9',
+		'D': '#F69B4F',
+		'C': '#F84837',
+	}
 	const statuses = ['S', 'A', 'B', 'D', 'C']
 
 	const data = FCASList
+	const renderFCASItem = ({ item, index }) => {
+		return (
+			<SwapableRow leftItems={[{ title: 'Favorite', icon: 'star' }]}>
+				<View
+					style={{
+						paddingVertical: 8,
+						paddingHorizontal: 20,
+						flexDirection: 'row',
+						alignItems: 'center',
+					}}
+				>
+					<View style={{ flex: 2 }}>
+						<AppText bold>{index + 1}</AppText>
+					</View>
+					<View style={{ flex: 3 }}>
+						<AppText bold>{item.name}</AppText>
+						<AppText typo="tiny" color="text3">
+							({item.symbol})
+						</AppText>
+					</View>
+					<View
+						style={{
+							flex: 3,
+							...globalStyles.flex.row,
+							...globalStyles.flex.center,
+						}}
+					>
+						<View
+							style={{
+								backgroundColor: colors[item.grade],
+								width: 28,
+								height: 20,
+								borderRadius: 6,
+								...globalStyles.flex.center,
+								marginHorizontal: 2,
+							}}
+						>
+							<AppText typo="tiny" color="whiteColor">
+								{item.grade}
+							</AppText>
+						</View>
+						<AppText style={{ marginHorizontal: 2 }} bold>
+							{item.score}
+						</AppText>
+					</View>
+					<View style={{ flex: 2 }}>
+						<Image
+							source={Images[item.chart]}
+							style={{ maxWidth: '100%' }}
+						/>
+					</View>
+				</View>
+				<View style={{ marginVertical: 12 }}>
+					{index + 1 !== data.length ? <HR /> : null}
+				</View>
+			</SwapableRow>
+		)
+	}
+
 
 	return (
 		<View style={globalStyles.screen}>
 			<RatingSortHeader />
 			<FlatList
 				data={data}
-				renderItem={({ item, index }) => {
-					return (
-						<SwapableRow leftItems={[{ title: 'Favorite', icon: 'star' }]}>
-							<View
-								style={{
-									paddingVertical: 8,
-									paddingHorizontal: 20,
-									flexDirection: 'row',
-									alignItems: 'center',
-								}}
-							>
-								<View style={{ flex: 2 }}>
-									<AppText bold>{index + 1}</AppText>
-								</View>
-								<View style={{ flex: 3 }}>
-									<AppText bold>{item.title}</AppText>
-									<AppText typo="tiny" color="text3">
-										({item.slug})
-									</AppText>
-								</View>
-								<View
-									style={{
-										flex: 3,
-										...globalStyles.flex.row,
-										...globalStyles.flex.center,
-									}}
-								>
-									<View
-										style={{
-											backgroundColor: colors[index % colors.length],
-											width: 28,
-											height: 20,
-											borderRadius: 6,
-											...globalStyles.flex.center,
-											marginHorizontal: 2,
-										}}
-									>
-										<AppText typo="tiny" color="whiteColor">
-											{statuses[index % statuses.length]}
-										</AppText>
-									</View>
-									<AppText style={{ marginHorizontal: 2 }} bold>
-										971
-									</AppText>
-								</View>
-								<View style={{ flex: 2 }}>
-									<Image
-										source={Images[item.chart]}
-										style={{ maxWidth: '100%' }}
-									/>
-								</View>
-							</View>
-							<View style={{ marginVertical: 12 }}>
-								{index + 1 !== data.length ? <HR /> : null}
-							</View>
-						</SwapableRow>
-					)
-				}}
-				keyExtractor={(_, i) => i.toString()}
+				renderItem={renderFCASItem}
+				keyExtractor={(item) => item.id.toString()}
 			/>
 		</View>
 	)
