f720aa93d61aca23998389ccb133f3d370104e3c
danial <dev.salahi@gmail.com>
2021-11-12 15:47:24 +0300

    added the market listing data from api to main provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,27 +1,40 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useState } from 'react'
 import useCoins from '../hooks/useCoins'
 import useFCASRating from '../hooks/useFCASRating'
-// import { } from './'
+import useMarketListing from '../hooks/useMarketListing'
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export const Context = createContext()
 
+const QueryConfigs = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+})
+
+
 const MainProvider = props => {
+
   const [state, setState] = useState({
     user: {},
     wallet: {}
   })
   const [render, setRender] = useState(false)
-
-
   const { coins, setCoin } = useCoins()
   const { FCASList } = useFCASRating()
+  const { MarketListing } = useMarketListing()
+
 
   const dispatch = value => setState({ ...state, ...value })
 
   return (
-    <Context.Provider value={{ ...state, coins, FCASList, setCoin, dispatch }}>
-      {props.children}
-    </Context.Provider>
+    <QueryClientProvider client={QueryConfigs}>
+      <Context.Provider value={{ ...state, coins, FCASList, MarketListing, setCoin, dispatch }}>
+        {props.children}
+      </Context.Provider>
+    </QueryClientProvider>
   )
 }
 
