2184660514b7c87305bc63a9629af257eed936be
danial <dev.salahi@gmail.com>
2021-11-13 16:31:12 +0300

    refactor main provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,40 +1,45 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import useCoins from '../hooks/useCoins'
 import useFCASRating from '../hooks/useFCASRating'
 import useMarketListing from '../hooks/useMarketListing'
-import { QueryClient, QueryClientProvider } from "react-query";
+import HttpService from '../services/HttpService';
 
 export const Context = createContext()
-
-const QueryConfigs = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-    },
-  },
-})
-
 
 const MainProvider = props => {
 
   const [state, setState] = useState({
     user: {},
-    wallet: {}
+    wallet: {},
+    coins: []
   })
   const [render, setRender] = useState(false)
-  const { coins, setCoin } = useCoins()
+  // const { coins, setCoin } = useCoins()
   const { FCASList } = useFCASRating()
   const { MarketListing } = useMarketListing()
 
+  useEffect(() => {
+    getCoins()
+  }, [])
 
+  const getCoins = () => {
+    new HttpService("", {
+      "uniqueId": "abc1",
+      "action": "supportedCoins",
+    }).Post(res => {
+      setState({ ...state, coins: res })
+    })
+  }
+
+  const setCoin = (value) => {
+    setState({ ...state, coins: value })
+  }
   const dispatch = value => setState({ ...state, ...value })
 
   return (
-    <QueryClientProvider client={QueryConfigs}>
-      <Context.Provider value={{ ...state, coins, FCASList, MarketListing, setCoin, dispatch }}>
-        {props.children}
-      </Context.Provider>
-    </QueryClientProvider>
+    <Context.Provider value={{ ...state, FCASList, MarketListing, setCoin, dispatch }}>
+      {props.children}
+    </Context.Provider>
   )
 }
 
