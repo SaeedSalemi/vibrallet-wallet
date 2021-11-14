af42e96c37f984cef13bf2674b216bf52f274a39
danial <dev.salahi@gmail.com>
2021-11-14 20:33:47 +0300

    removed previus app version of the splash screen

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { globalStyles } from './src/config/styles'
 import { Provider } from 'react-redux'
 import configStore from './src/redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
-import SplashScreen from './src/screens/SplashScreen'
+// import SplashScreen from './src/screens/SplashScreen'
 import { Keyboard, TouchableWithoutFeedback } from 'react-native'
 import HideKeyboard from './src/utils/HideKeyboard'
 import InAppNotificaiton from './src/components/common/AppNotification/InAppNotification'
@@ -27,21 +27,21 @@ const QueryConfigs = new QueryClient({
 export default function App() {
 	return (
 		<Provider store={store}>
-			<PersistGate loading={<SplashScreen />} persistor={persistor}>
-				<QueryClientProvider client={QueryConfigs}>
-					<MainProvider>
-						<SafeAreaProvider
-							style={{ backgroundColor: globalStyles.Colors.bckColor }}
-						>
-							<InAppNotificaiton />
-							<StatusBar backgroundColor={globalStyles.Colors.bckColor} barStyle="light-content" />
-							<HideKeyboard>
-								<RootNavigation />
-							</HideKeyboard>
-						</SafeAreaProvider>
-					</MainProvider>
-				</QueryClientProvider>
-			</PersistGate>
+			{/* <PersistGate loading={<SplashScreen />} persistor={persistor}> */}
+			<QueryClientProvider client={QueryConfigs}>
+				<MainProvider>
+					<SafeAreaProvider
+						style={{ backgroundColor: globalStyles.Colors.bckColor }}
+					>
+						<InAppNotificaiton />
+						<StatusBar backgroundColor={globalStyles.Colors.bckColor} barStyle="light-content" />
+						<HideKeyboard>
+							<RootNavigation />
+						</HideKeyboard>
+					</SafeAreaProvider>
+				</MainProvider>
+			</QueryClientProvider>
+			{/* </PersistGate> */}
 		</Provider>
 	)
 }
