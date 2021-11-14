77e5418916f975cbfa372928294f1e47f0db8c9f
danial <dev.salahi@gmail.com>
2021-11-14 14:56:01 +0300

    disable useCoins on MarketScreen

--- src/screens/app/MarketStack/MarketTab.js
+++ src/screens/app/MarketStack/MarketTab.js
@@ -2,15 +2,11 @@ import React, { useContext } from 'react'
 import { View } from 'react-native'
 import SortHeader from '../../../components/Market/SortHeader'
 import { globalStyles } from '../../../config/styles'
-import useCoins from '../../../hooks/useCoins'
-// import { coins } from '../HomeStack/CreatePriceAlertScreen'
 import { Context } from '../../../context/Provider'
 import MarketData from './MarketData'
 
 export default function MarketTabScreen() {
 	const { MarketListing: coins } = useContext(Context)
-	// const coins = useCoins()
-
 	return (
 		<View style={globalStyles.screen}>
 			<SortHeader />
