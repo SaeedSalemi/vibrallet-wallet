542c87325d8d9508a1e82b5b738eeaaa3667eea8
danial <dev.salahi@gmail.com>
2021-11-18 09:42:57 +0300

    added functionality: cache, balance, supported coins

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,13 +1,13 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { createContext, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 import bitcoinManager from '../blockchains/BitcoinManager';
 import bscManager from '../blockchains/BscManager';
 import ethManager from '../blockchains/EthManager';
-import useCoins from '../hooks/useCoins'
 import useFCASRating from '../hooks/useFCASRating'
 import useMarketListing from '../hooks/useMarketListing'
 import HttpService from '../services/HttpService';
-import { setToStorage, getFromStorage } from '../utils/Functions';
+import { setToStorage } from '../utils/Functions';
 
 export const Context = createContext()
 
@@ -23,22 +23,28 @@ const MainProvider = props => {
   const { FCASList } = useFCASRating()
   const { MarketListing } = useMarketListing()
 
+  const wallet = useSelector(state => {
+    state.wallets.data ? state.wallets.data[0] : null
+  }
+  )
+
   useEffect(() => {
-
-
     AsyncStorage.getItem("supportedCoins")
       .then(result => {
-        let supportedCoinData = JSON.parse(result)
-        if (supportedCoinData && supportedCoinData.length > 0) {
-          setState({ ...state, coins: response })
+        if (result !== null) {
+          let supportedCoinData = JSON.parse(result)
+          if (supportedCoinData && supportedCoinData.length > 0) {
+            setState({ ...state, coins: supportedCoinData })
+          }
         } else {
           new HttpService("", {
             "uniqueId": "abc1",
             "action": "supportedCoins",
           }).Post(response => {
             if (response.length > 0) {
-              setState({ ...state, coins: response })
-              setToStorage("supportedCoins", JSON.stringify(response))
+              const setBalanceToResponse = response.forEach(coin => coin.balance = 0)
+              setState({ ...state, coins: setBalanceToResponse })
+              setToStorage("supportedCoins", JSON.stringify(setBalanceToResponse))
             }
           })
         }
@@ -52,8 +58,45 @@ const MainProvider = props => {
   }
   const dispatch = value => setState({ ...state, ...value })
 
+
+  const getCoinBalance = coinSymbol => {
+    if (!coinSymbol)
+      return 0
+
+    let balance = 0
+    AsyncStorage.getItem("supportedCoins").then(data => {
+      if (data !== null) {
+        const parsedCoins = JSON.parse(data);
+        for (let coin of parsedCoins) {
+          if (coin["symbol"] === coinSymbol)
+            if (coin.hasOwnProperty("balance")) {
+              balance = parseFloat(coin["balance"]).toFixed(3)
+            } else {
+
+              if (wallet) {
+                let selectedCoin = state.coinManager[coinSymbolxxz];
+                if (typeof selectedCoin.getWalletFromMnemonic === "function") {
+                  selectedCoin.getWalletFromMnemonic(wallet.backup)
+                    .then(wallet => {
+                      selectedCoin.getBalance(wallet?.address, false).then(result => {
+                        balance = parseFloat(result).toFixed(3)
+                      })
+                    })
+                    .catch(ex => console.error('balance wallet error', ex))
+                }
+              }
+
+            }
+        }
+      }
+    })
+
+    return balance
+  }
+
+
   return (
-    <Context.Provider value={{ ...state, FCASList, MarketListing, setCoin, dispatch }}>
+    <Context.Provider value={{ ...state, getCoinBalance, FCASList, MarketListing, setCoin, dispatch }}>
       {props.children}
     </Context.Provider>
   )
