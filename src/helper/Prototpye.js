0e3b1af781f804478f0fe18a50d2b4d3f5e5d395
danial <dev.salahi@gmail.com>
2021-11-21 14:39:43 +0300

    added prototype helper

---
+++ src/helper/Prototpye.js
@@ -0,0 +1,9 @@
+
+Object.defineProperty(String.prototype, "isEmail", {
+  value: function () {
+    const email = this
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+})
+
