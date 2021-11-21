a648fd92fbcff30135254830b85b56b8256c6e15
danial <dev.salahi@gmail.com>
2021-11-21 14:40:17 +0300

    added prototype into the index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler'
 
 import App from './App'
 import { AppRegistry } from 'react-native'
+import './src/helper/Prototpye'
 
 AppRegistry.registerComponent('main', () => App)
 // It also ensures that whether you load the app in Expo Go or in a native build,
