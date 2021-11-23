e6a4ec314bbde97ba43be497475159eb0c1e1528
danial <dev.salahi@gmail.com>
2021-11-23 14:42:49 +0300

    added use Effect

--- src/screens/app/MarketStack/MarketTab.js
+++ src/screens/app/MarketStack/MarketTab.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { View } from 'react-native'
 import SortHeader from '../../../components/Market/SortHeader'
 import { globalStyles } from '../../../config/styles'
