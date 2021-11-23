eaef26c23bf784bdcee1ceb389f25287b5006e2d
danial <dev.salahi@gmail.com>
2021-11-23 23:45:51 +0300

    refactor barchart

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -5,21 +5,23 @@ import AppText from '../common/AppText'
 import Bar from './components/Bar'
 // const percent = [100, 75, 50, 25, 0]
 import { BarChart, Grid, YAxis, XAxis } from 'react-native-svg-charts'
-
+import * as shape from 'd3-shape'
+// import {Gradient} from "@/scree"
 const _BarChart = ({ data }) => {
   const fill = 'rgb(134, 65, 244)'
-  const _data = [0, 0, 0]
+  const _data = [10, 50, 10, 5, 10]
   const contentInset = { top: 5, bottom: 10 }
-  const x = ['ETH', 'BTC', 'BNB']
+  const x = ['ETH', 'BTC', 'BNB', "mm", "dd"]
   const percent = [0, 25, 50, 75, 100]
 
   return (
-    <View style={{ flexDirection: 'column' }}>
+    <View style={{ flexDirection: 'column', }}>
 
-      <View style={{ flexDirection: "row", height: 110 }}>
+      <View style={{ flexDirection: "row", height: 150, }}>
         <YAxis
           data={percent}
-          contentInset={contentInset}
+          contentInset={{ bottom: 30, top: -20 }}
+          style={{ height: "100%", backgourndColor: "red" }}
           svg={{
             fill: 'grey',
             fontSize: 10,
@@ -28,26 +30,41 @@ const _BarChart = ({ data }) => {
           // formatLabel={(value, index) => `${percent[index] === undefined ? '' : percent[index]}`}
           formatLabel={(value, index) => `${percent[index] === undefined ? '' : `${percent[index]}%`}`}
         />
-        <BarChart
-          // spacingInner={.8}
-          style={{ flex: 1, marginLeft: 16 }} data={_data} svg={{ fill }} contentInset={contentInset}>
-          <Grid />
-        </BarChart>
+        <View style={{ flexDirection: "column", flex: 1 }}>
+          <BarChart
+            // spacingInner={.2}
+            spacingInner={0.4}
+            spacingOuter={0.1}
+            // gridMin={-100}
+            // gridMax={120}
+
+            animate
+            curve={shape.curveNatural}
+
+            style={{ flex: 1, height: 150 }} data={_data} svg={{
+              fill
+              // strokeLinejoin: "round",
+              // strokeLinecap: "round",
+              // strokeWidth: 5,d3-shape
+              // strokeDasharray: [14, 4],
+              // stroke: 'red'
+            }} contentInset={contentInset}>
+            <Grid />
+          </BarChart>
+          <View style={{ height: 10 }} />
+          <XAxis
+            // style={{ marginHorizontal: -10 }}
+            data={x}
+            formatLabel={(value, index) => x[index]}
+            contentInset={{ left: 20, right: 20 }}
+            // contentInset={contentInset}
+            svg={{ fontSize: 10, fill: 'gray' }}
+          />
+        </View>
       </View>
 
-      <XAxis
-        data={x}
-        contentInset={contentInset}
-        svg={{
-          fill: 'grey',
-          fontSize: 10,
-        }}
-        style={{ height: 20, paddingTop: 5, }}
-        // formatLabel={(value, index) => value}
-        formatLabel={(value, index) => x[index]}
-        numberOfTicks={3}
-      // formatLabel={(value) => `${value}`}
-      />
+
+
     </View>
 
   )
