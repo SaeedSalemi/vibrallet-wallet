eb55241b22950331f8ed1b6912ed50dac3416381
danial <dev.salahi@gmail.com>
2021-11-28 20:16:56 +0300

    market web view widget

---
+++ src/components/MarketWebView/MarketWebView.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { WebView } from 'react-native-webview';
+import PropTypes from 'prop-types';
+
+const MarketWebView = (props) => {
+
+  return (
+    <WebView
+      style={{ width: "100%" }}
+      source={{ uri: `https://coinmarketcap.com/currencies/${props.name}` }}
+    />
+  )
+
+}
+
+
+MarketWebView.propTypes = {
+  name: PropTypes.string,
+}
+
+MarketWebView.defaultProps = {
+  name: 'plutonium',
+}
+
+export default MarketWebView;
