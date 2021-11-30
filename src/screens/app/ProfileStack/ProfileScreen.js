c097503116a97632cb1d8cdb04721c4818712385
danial <dev.salahi@gmail.com>
2021-11-30 19:33:59 +0300

    backup and logout

--- src/screens/app/ProfileStack/ProfileScreen.js
+++ src/screens/app/ProfileStack/ProfileScreen.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AppButton from '../../../components/common/AppButton'
 import Screen from '../../../components/Screen'
 import { setUser } from '../../../utils/storage'
-import { View } from 'react-native'
+import { BackHandler, View } from 'react-native'
 import ProfileInfo from '../../../components/Profile/ProfileInfo'
 import ProfileDetailsSection from '../../../components/Profile/ProfileDetailsSection'
 import UserCoins from '../../../components/Profile/UserCoins'
@@ -13,13 +13,58 @@ import Header from '../../../components/Header/Header'
 import { routes } from '../../../config/routes'
 import { useDispatch } from 'react-redux'
 import { setLoggedIn } from '../../../redux/modules/appSettings'
-
+import RNFS from 'react-native-fs'
+import { encrypt } from '../../../utils/Functions'
+import { showMessage } from 'react-native-flash-message'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import walletManager from '../../../blockchains/walletManager'
 export default function ProfileScreen() {
 	const dispatch = useDispatch()
+	const [backup, setBackup] = useState('')
 
-	const handleLogout = () => {
-		setUser(null)
-		dispatch(setLoggedIn(false))
+	useEffect(() => {
+		const words = walletManager.generateMnemonic()
+		setBackup(words)
+	}, [])
+
+	const handleLogout = async () => {
+
+		const path = RNFS.DownloadDirectoryPath + `/vibranium-backup-${new Date().getTime()}.json`;
+		RNFS.writeFile(path, JSON.stringify(encrypt(backup)), 'utf8')
+			.then((success) => {
+				showMessage({
+					message: 'Your wallet backup has been created in your downloads',
+					description: null,
+					type: 'success',
+					icon: null,
+					duration: 3000,
+					style: { backgroundColor: "#16a085" },
+					position: 'top'
+				})
+				AsyncStorage.clear().then(() => {
+					showMessage({
+						message: 'Your are logged out.',
+						description: null,
+						type: 'danger',
+						icon: null,
+						duration: 3000,
+						style: { backgroundColor: "#16a085" },
+						position: 'top'
+					})
+
+				}).catch(err => {
+					console.log('removing for the app', err.message);
+				})
+				// setUser(null)
+				// dispatch(setLoggedIn(false))
+				BackHandler.exitApp()
+
+
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+
 	}
 
 	return (
