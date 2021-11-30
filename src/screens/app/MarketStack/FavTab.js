c5bfe907a5410daa910e806fa3cd4b1cc0ce4194
danial <dev.salahi@gmail.com>
2021-11-30 12:04:35 +0300

    removed server side xhr fav list.

--- src/screens/app/MarketStack/FavTab.js
+++ src/screens/app/MarketStack/FavTab.js
@@ -10,44 +10,7 @@ import { Context } from '../../../context/MarketProvider'
 
 export default function FavTabScreen() {
 
-	// const { coins } = useContext(Context)
-	// const [state, setState] = useState([])
 	const { favCoins } = useContext(Context)
-
-	// useEffect(() => {
-
-	// 	AsyncStorage.getItem("marketFavCoins").then(coins => {
-	// 		if (coins)
-	// 			setState(JSON.parse(coins))
-	// 	})
-	// }, [])
-
-	// useEffect(() => {
-	// 	new HttpService(
-	// 		"", {
-	// 		"uniqueId": "abc",
-	// 		"action": "getFavoriteCurrencies",
-	// 		"data": {
-	// 			"kind": "MARKET"
-	// 		}
-	// 	}
-	// 	).Post(response => {
-	// 		if (response) {
-	// 			// console.log('response from the fav')
-	// 			// TODO: develop the FAV for fcas with API
-	// 			// showMessage({
-	// 			// 	message: `In order to add coin to your favorite list you have sign in`,
-	// 			// 	description: null,
-	// 			// 	type: 'success',
-	// 			// 	icon: null,
-	// 			// 	duration: 8000,
-	// 			// 	style: { backgroundColor: "#e74c3c" },
-	// 			// 	position: 'top'
-	// 			// })
-	// 		}
-	// 	})
-	// }, [])
-
 	return (
 		<View style={globalStyles.screen}>
 			<SortHeader />
