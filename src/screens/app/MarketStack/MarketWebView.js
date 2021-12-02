59def19ae79e2e5c684dca7f39fd37f8ba734ce3
danial <dev.salahi@gmail.com>
2021-12-02 11:26:19 +0300

    set coin to lower case and set -

--- src/screens/app/MarketStack/MarketWebView.js
+++ src/screens/app/MarketStack/MarketWebView.js
@@ -1,50 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {
-  Image,
-  Modal,
   ScrollView,
-  TouchableOpacity,
   View,
-  ViewPropTypes,
 } from 'react-native'
-import { routes } from '../../../config/routes'
-import { globalStyles } from '../../../config/styles'
-import AppText from '../../../components/common/AppText'
-import Header from '../../../components/Header/Header'
 import Screen from '../../../components/Screen'
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
-import { Images } from '../../../assets'
-import MarketCoinDetailItems from '../../../components/Market/MarketCoinDetailItems'
-import AppButton from '../../../components/common/AppButton'
-import { useDispatch, useSelector } from 'react-redux'
-import { setModal } from '../../../redux/modules/appSettings'
-import SetAlert from '../../../components/Market/SerAlert'
 import { useNavigation } from '@react-navigation/core'
-// import TradingViewWidget from '../../../components/TradingViewWidget/TradingViewWidget'
-import MarketWebViewWidget from '../../../components/MarketWebView/MarketWebView'
 import { WebView } from 'react-native-webview';
 
 
 export default function MarketWebView({ route, navigation }) {
   const { coin } = route.params || {}
 
-  console.log('web view', coin)
   const { navigate } = useNavigation()
-
-  console.log('coin is here', coin.name);
+  let name = coin.name.replace(/\s+/g, '-').toLowerCase();
   return (
     <Screen >
       <ScrollView>
         <View style={{ marginVertical: 2, height: 750 }}>
-          {/* <MarketWebViewWidget name={coin.name} /> */}
           <WebView
-            source={{ uri: `https://coinmarketcap.com/currencies/${coin.name}/` }}
+            source={{ uri: `https://coinmarketcap.com/currencies/${name}/` }}
             style={{ marginTop: 20 }}
           />
         </View>
-
       </ScrollView>
 
     </Screen>
