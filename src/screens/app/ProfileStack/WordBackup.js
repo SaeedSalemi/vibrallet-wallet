10be2a82354231117c5ac826ae15935534e966b4
danial <dev.salahi@gmail.com>
2021-12-02 12:29:06 +0300

    generate wallet info from wordbackup

--- src/screens/app/ProfileStack/WordBackup.js
+++ src/screens/app/ProfileStack/WordBackup.js
@@ -14,16 +14,80 @@ import { showMessage } from "react-native-flash-message";
 import { routes } from '../../../config/routes'
 import RNFS from 'react-native-fs'
 import { encrypt } from '../../../utils/Functions'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import HttpService from '../../../services/HttpService'
+import bitcoinManager from '../../../blockchains/BitcoinManager'
+import ethManager from '../../../blockchains/EthManager'
+import bscManager from '../../../blockchains/BscManager'
 
 export default function WordBackup({ navigation }) {
 	const dispatch = useDispatch()
 	const { navigate } = navigation
 	const [backup, setBackup] = useState('')
+	const [state, setState] = useState({
+		preDefinedCoinsColors: { BTC: '#F47169', BNB: '#FFCC01', ETH: '#7037C9', },
+	})
 
 	useEffect(() => {
 		const words = WalletManager.generateMnemonic()
+		console.log('debug words', words)
 		setBackup(words)
+		supportedCoins()
 	}, [])
+
+	const supportedCoins = () => {
+
+		try {
+			new HttpService("", {
+				"uniqueId": "abc1",
+				"action": "supportedCoins",
+			}).Post(async (response) => {
+
+				try {
+					if (response) {
+						// if (wallet) {
+						const items = response
+						for (let item of items) {
+							item.balance = 0
+							item.color = state.preDefinedCoinsColors[item.symbol]
+							item.hide = false
+							item.fav = false
+							if (item.symbol === 'BTC') {
+								const coininfo = await bitcoinManager.getWalletFromMnemonic(backup)
+								item.publicKey = coininfo.publicKey
+								item.privateKey = coininfo.privateKey
+								item.address = coininfo.address
+								// item.balance = await bitcoinManager.getBalance(item.address)
+							}
+							if (item.symbol.toUpperCase() === 'ETH') {
+								const coininfo = await ethManager.getWalletFromMnemonic(backup)
+								item.publicKey = coininfo.publicKey
+								item.privateKey = coininfo.privateKey
+								item.address = coininfo.address
+								item.balance = await ethManager.getBalance(item.address)
+							}
+							if (item.symbol.toUpperCase() === 'BNB') {
+								const coininfo = await bscManager.getWalletFromMnemonic(backup)
+								item.publicKey = coininfo.publicKey
+								item.privateKey = coininfo.privateKey
+								item.address = coininfo.address
+								item.balance = await bscManager.getBalance(item.address)
+							}
+						}
+						AsyncStorage.setItem("supportedCoins", JSON.stringify(items)).then().catch()
+					}
+				} catch (error) {
+					console.log('debug error', error)
+				}
+			})
+
+
+		} catch (err) {
+			console.log(err)
+		}
+
+	}
+
 
 	const handleAddWallet = () => {
 		dispatch(finalCreateWallet(backup))
@@ -47,7 +111,7 @@ export default function WordBackup({ navigation }) {
 
 
 	const handleBackup = () => {
-		const path = RNFS.DownloadDirectoryPath + '/vibranium-backup.json';
+		const path = RNFS.DownloadDirectoryPath + `/vibranium-backup-${new Date().getTime()}.json`;
 		RNFS.writeFile(path, JSON.stringify(encrypt(backup)), 'utf8')
 			.then((success) => {
 				showMessage({
