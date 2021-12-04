395899482b1375ae759d993377acc9215cb04a23
danial <dev.salahi@gmail.com>
2021-12-04 14:10:50 +0300

    refactor useReduxWallet

--- src/hooks/useReduxWallet.js
+++ src/hooks/useReduxWallet.js
@@ -1,26 +1,40 @@
-import { useSelector } from 'react-redux'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import bitcoinManager from '../blockchains/BitcoinManager'
 import bscManager from '../blockchains/BscManager'
 import ethManager from '../blockchains/EthManager'
 
-export const useReduxWallet = (coin) => {
+export const useReduxWallet = async (coin) => {
   if (!coin)
     return {}
 
-  const wallet = useSelector(state => { state.wallets.data ? state.wallets.data[0] : null })
-  const coinSelector = { ETH: ethManager, BNB: bscManager, BTC: bitcoinManager }
-
-  let balance = 0
-  if (wallet) {
-    coinSelector[coin.symbol].getBalance(coin.address, false).then(result => {
-      balance = result
-    }).catch(err => {
-      console.log('useReduxWallet', err)
-    })
+  const getStoredMnemonic = async () => {
+    const persist = await AsyncStorage.getItem('persist:root')
+    if (persist !== null) {
+      let item = JSON.parse(persist)
+      if (item !== null) {
+        let wallets = JSON.parse(item["wallets"])
+        return wallets.data ? wallets.data[0] : null
+      }
+    }
   }
 
-  return {
-    balance
+  const getBalanceFromMnemonic = async (coin) => {
+    const coinSelector = { ETH: ethManager, BNB: bscManager, BTC: bitcoinManager }
+    const balance = await coinSelector[coin.symbol].getBalance(coin.address, false)
+    console.log('getBalanceFromMnemonic debug', coin.symbol, `Balance is  ${balance}`)
+    return balance
   }
+
+  const mnemonic = await getStoredMnemonic()
+
+  if (mnemonic) {
+    const balance = await getBalanceFromMnemonic(coin)
+    console.log('real balance', balance)
+    return {
+      balance
+    }
+  } else
+    return {}
+
 }
 
