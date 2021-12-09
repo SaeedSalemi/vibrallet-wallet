c6845319badc6baa3e21a54473cebb90c8116d3e
danial <dev.salahi@gmail.com>
2021-12-09 21:47:30 +0300

    set username, load user profile, copy user name functionality

--- src/components/ReceiveScreen/UsernameScreen.js
+++ src/components/ReceiveScreen/UsernameScreen.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Image, ScrollView } from 'react-native'
+import React, { useContext } from 'react'
+import { View, Image, ScrollView, TouchableOpacity } from 'react-native'
 import { Images } from '../../assets'
 import { globalStyles } from '../../config/styles'
 import AppButton from '../common/AppButton'
@@ -7,9 +7,15 @@ import AppIcon from '../common/AppIcon'
 import AppInput from '../common/AppInput/AppInput'
 import AppText from '../common/AppText'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+import { Context } from '../../context/Provider'
+import Entypo from 'react-native-vector-icons/Entypo'
+import { showMessage } from 'react-native-flash-message'
+import Clipboard from '@react-native-community/clipboard'
 
 export default function UsernameScreen({ route }) {
 	const { coin } = route.params || {}
+
+	const { user: userProvider, userProfile } = useContext(Context)
 
 	return (
 		<View style={{ ...globalStyles.gapScreen }}>
@@ -38,13 +44,43 @@ export default function UsernameScreen({ route }) {
 					</View>
 				</View>
 				<View style={{ ...globalStyles.flex.center }}>
-					<Image source={Images.avatar3} style={{ marginVertical: 12 }} />
+					{/* <Image source={Images.avatar3} style={{ marginVertical: 12 }} /> */}
+					{userProfile ? <Image source={{ uri: `${userProfile}` }} style={{ width: 30, height: 30, borderRadius: 50 }} /> : <Entypo
+						name="user"
+						size={60}
+						color={globalStyles.Colors.inputColor2}
+					/>}
 					<AppText bold typo="md">
-						{'Login'}
+						{userProvider.username ? userProvider.username : 'Login'}
 					</AppText>
-					<AppText typo="dot" color="text3">
-						Click to copy
-					</AppText>
+					<TouchableOpacity onPress={() => {
+						if (userProvider.username) {
+							Clipboard.setString(userProvider.username)
+							showMessage({
+								message: `Copied to your Clipboard`,
+								description: null,
+								type: 'success',
+								icon: null,
+								duration: 2000,
+								style: { backgroundColor: "#27BF74" },
+								position: 'top'
+							})
+						} else {
+							showMessage({
+								message: `User name is not set.`,
+								description: null,
+								type: 'success',
+								icon: null,
+								duration: 2500,
+								style: { backgroundColor: "#F84837" },
+								position: 'top'
+							})
+						}
+					}}>
+						<AppText typo="dot" color="text3">
+							Click to copy
+						</AppText>
+					</TouchableOpacity>
 				</View>
 				<View style={{ marginVertical: 30, flex: 1 }}>
 					<AppInput
