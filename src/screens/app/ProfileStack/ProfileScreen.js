79977850d6b6bbe0f62fd68fe63f5f05d3cac5fe
danial <dev.salahi@gmail.com>
2021-12-22 19:01:12 +0300

    backup & logout task: getting backup, redirect to welcome, press handler, clean storage

--- src/screens/app/ProfileStack/ProfileScreen.js
+++ src/screens/app/ProfileStack/ProfileScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import AppButton from '../../../components/common/AppButton'
 import Screen from '../../../components/Screen'
-import { setUser } from '../../../utils/storage'
+// import { setUser } from '../../../utils/storage'
 import { BackHandler, View } from 'react-native'
 import ProfileInfo from '../../../components/Profile/ProfileInfo'
 import ProfileDetailsSection from '../../../components/Profile/ProfileDetailsSection'
@@ -11,60 +11,43 @@ import ProfileMainRouteSection from '../../../components/Profile/ProfileMainRout
 import { globalStyles } from '../../../config/styles'
 import Header from '../../../components/Header/Header'
 import { routes } from '../../../config/routes'
-import { useDispatch } from 'react-redux'
-import { setLoggedIn } from '../../../redux/modules/appSettings'
-import RNFS from 'react-native-fs'
-import { encrypt } from '../../../utils/Functions'
+// import { useDispatch } from 'react-redux'
+// import { setLoggedIn } from '../../../redux/modules/appSettings'
+// import RNFS from 'react-native-fs'
+import { encrypt, gettingBackup } from '../../../utils/Functions'
 import { showMessage } from 'react-native-flash-message'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import walletManager from '../../../blockchains/walletManager'
-export default function ProfileScreen() {
-	const dispatch = useDispatch()
-	const [backup, setBackup] = useState('')
+// import walletManager from '../../../blockchains/walletManager'
+export default function ProfileScreen({ navigation }) {
+	const [isLoggingOut, setIsLoggingOut] = useState(false)
 
-	useEffect(() => {
-		const words = walletManager.generateMnemonic()
-		setBackup(words)
-	}, [])
+	const getStoredMnemonic = async () => {
+		const persist = await AsyncStorage.getItem('persist:root')
+		if (persist !== null) {
+			let item = JSON.parse(persist)
+			if (item !== null) {
+				let wallets = JSON.parse(item["wallets"])
+				return wallets.data ? wallets.data[0] : null
+			}
+		}
+	}
 
 	const handleLogout = async () => {
-
-		const path = RNFS.DownloadDirectoryPath + `/vibranium-backup-${new Date().getTime()}.json`;
-		RNFS.writeFile(path, JSON.stringify(encrypt(backup)), 'utf8')
-			.then((success) => {
-				showMessage({
-					message: 'Your wallet backup has been created in your downloads',
-					description: null,
-					type: 'success',
-					icon: null,
-					duration: 3000,
-					style: { backgroundColor: "#16a085" },
-					position: 'top'
-				})
-				AsyncStorage.clear().then(() => {
-					showMessage({
-						message: 'Your are logged out.',
-						description: null,
-						type: 'danger',
-						icon: null,
-						duration: 3000,
-						style: { backgroundColor: "#16a085" },
-						position: 'top'
-					})
-
-				}).catch(err => {
-					console.log('removing for the app', err.message);
-				})
-				// setUser(null)
-				// dispatch(setLoggedIn(false))
-				BackHandler.exitApp()
-
-
-			})
-			.catch((err) => {
-				console.log(err.message);
-			});
-
+		setIsLoggingOut(true)
+		const { backup: _mnemonic } = await getStoredMnemonic()
+		gettingBackup(_mnemonic)
+		await AsyncStorage.clear()
+		showMessage({
+			message: 'Your are logged out.',
+			description: null,
+			type: 'danger',
+			icon: null,
+			duration: 3000,
+			style: { backgroundColor: "#16a085" },
+			position: 'top'
+		})
+		navigation.navigate(routes.welcome)
+		setIsLoggingOut(false)
 	}
 
 	return (
@@ -81,6 +64,7 @@ export default function ProfileScreen() {
 				<ProfileMainRouteSection />
 				<View style={{ paddingHorizontal: 16 }}>
 					<AppButton
+						loading={isLoggingOut}
 						typo="xs"
 						title="Backup and Log Out"
 						textStyle={{ color: globalStyles.Colors.failure }}
