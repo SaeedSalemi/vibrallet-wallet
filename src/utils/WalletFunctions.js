b238cc7fd03642e824a69f8985d90e608a8e0b39
danial <dev.salahi@gmail.com>
2021-12-27 15:25:46 +0300

    added getCoinBalance function in the WalletFunction

--- src/utils/WalletFunctions.js
+++ src/utils/WalletFunctions.js
@@ -1,4 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import bitcoinManager from '../blockchains/BitcoinManager'
+import bscManager from '../blockchains/BscManager'
+import ethManager from '../blockchains/EthManager'
 import { persist_root } from './../config/async-storage.json'
 export const getStoredMnemonic = async () => {
   const persist = await AsyncStorage.getItem(persist_root)
@@ -23,4 +26,25 @@ export const checkExistsWallet = async () => {
       return false
     }
   }
+}
+
+export const getCoinBalance = async (coin) => {
+  if (!coin)
+    return 0
+
+  if (checkExistsWallet()) {
+    try {
+      let balance = 0
+      const coinManager = { ETH: ethManager, BNB: bscManager, BTC: bitcoinManager }
+      let selectedCoin = coinManager[coin.symbol];
+      balance = await selectedCoin.getBalance(coin.address, false);
+      if (!balance)
+        return 0;
+
+      return balance
+    } catch (err) {
+      console.log('Weallet Function get Coin Balance from Network ', err)
+    }
+  } else
+    return 0
 }
