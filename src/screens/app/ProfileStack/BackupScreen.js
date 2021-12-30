d297ad253d854d2e70950de5f6d82547b7a5f584
danial <dev.salahi@gmail.com>
2021-12-30 16:39:32 +0300

    fully refactor the backup in the profile stack

--- src/screens/app/ProfileStack/BackupScreen.js
+++ src/screens/app/ProfileStack/BackupScreen.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { View, Image, TouchableOpacity, Text } from 'react-native'
+import React, { useEffect, useState, useContext, useLayoutEffect } from 'react'
+import { View, TouchableOpacity, Pressable } from 'react-native'
 import { useDispatch } from 'react-redux'
-import { Images } from '../../../assets'
+import AppButton from '../../../components/common/AppButton'
 import AppText from '../../../components/common/AppText'
 import HR from '../../../components/common/HR/HR'
 import Screen from '../../../components/Screen'
@@ -11,50 +11,128 @@ import WalletManager from '../../../blockchains/walletManager'
 import Clipboard from '@react-native-community/clipboard'
 import { showMessage } from "react-native-flash-message";
 import { routes } from '../../../config/routes'
-import RNFS from 'react-native-fs'
-import { encrypt } from '../../../utils/Functions'
-export default function BackupScreen({ navigation }) {
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import HttpService from '../../../services/HttpService'
+import bitcoinManager from '../../../blockchains/BitcoinManager'
+import ethManager from '../../../blockchains/EthManager'
+import bscManager from '../../../blockchains/BscManager'
+import { Context } from '../../../context/Provider'
+import QRCode from 'react-native-qrcode-svg'
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import {
+  GDrive,
+  MimeTypes
+} from "@robinbobin/react-native-google-drive-api-wrapper";
+import { gettingBackup } from '../../../utils/Functions'
+import { getStoredMnemonic, checkExistsWallet } from '../../../utils/WalletFunctions'
+
+export default function WordBackup({ navigation }) {
+  const { setCoinsToSupport } = useContext(Context)
+  const [googleInformation, setGoogleInformation] = useState({})
+  const [backupPlan, setBackupPlan] = useState()
+
+  const [loading, setLoading] = useState(false)
 
   const dispatch = useDispatch()
   const { navigate } = navigation
   const [backup, setBackup] = useState('')
+  const [state, setState] = useState({
+    preDefinedCoinsColors: { BTC: '#F47169', BNB: '#FFCC01', ETH: '#7037C9', },
+  })
 
-  useEffect(() => {
-    // const words = WalletManager.generateMnemonic()
-    // setBackup(words)
+  GoogleSignin.configure({
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'], // [Android] what API you want to access on behalf 
+    webClientId: '226354431357-ep3eo9tnlau5vil2s5cph88igtca45ut.apps.googleusercontent.com', // client ID of type
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    hostedDomain: '', // specifies a hosted domain restriction
+    forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+    accountName: '', // [Android] specifies an account name on the device that should be used
+  });
 
+  useLayoutEffect(() => {
+
+    checkExistsWallet().then(wallet => {
+    }).catch(err => { console.log('splash screen catch', err) })
   }, [])
 
-  const handleAddWallet = () => {
-    // dispatch(finalCreateWallet(backup))
-    // showMessage({
-    //   message: 'Your wallet has been created successfully',
-    //   description: null,
-    //   type: 'success',
-    //   icon: null,
-    //   duration: 2000,
-    //   style: { backgroundColor: "#16a085" },
-    //   position: 'top'
-    // })
-    // navigation.navigate(routes.appTab)
-  }
+  const signIn = async () => {
+
+    try {
+      const hasPlayServices = await GoogleSignin.hasPlayServices()
+      if (hasPlayServices) {
+        const userInfo = await GoogleSignin.signIn();
+        console.log('user info', userInfo)
+        this.setGoogleInformation({ userInfo });
+      }
+      // const gdrive = new GDrive();
+
+      // gdrive.accessToken = (await GoogleSignin.getTokens()).accessToken;
+      // console.log('GDRIVE', await gdrive.files.list());
+
+      // const id = (await gdrive.files.newMultipartUploader()
+      // 	.setData([1, 2, 3, 4, 5], MimeTypes.BINARY)
+      // 	.setRequestBody({
+      // 		name: "multipart_bin"
+      // 	})
+      // 	.execute()
+      // ).id;
+      // console.log('kossher ', await gdrive.files.getBinary(id));
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        alert('user cancelled the login flow');
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('IN_PROGRESS')
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('Play services is not available');
+      } else {
+        // some other error happened
+        alert('Something went wrong');
+        console.log('google went wrong!', error)
+      }
+    }
+  };
 
   const backupList = backup.split(' ').map((word, i) => `${i + 1}. ${word}`)
 
   const handleCopy = () => {
     Clipboard.setString(backup)
+    showMessage({
+      message: `Backup copied to clipboard`,
+      description: null,
+      type: 'success',
+      icon: null,
+      duration: 1000,
+      style: { backgroundColor: "#6BC0B1" },
+      position: 'top'
+    })
   }
 
 
   const handleBackup = () => {
+    gettingBackup(backup)
   }
-
 
 
   return (
     <Screen edges={['bottom']} style={{ ...globalStyles.gapScreen }}>
-      <View style={{ paddingVertical: 18, ...globalStyles.flex.center }}>
-        <Image source={Images.qrCode} />
+      <View style={{
+        ...globalStyles.flex.center,
+        paddingHorizontal: 12,
+        paddingVertical: 18,
+      }}>
+        <View style={{ backgroundColor: globalStyles.Colors.inputColor, padding: 10, borderRadius: 12, }}>
+          {backup ? <QRCode
+            size={150}
+            value={backup}
+            logoBackgroundColor="transparent"
+          /> : <></>}
+        </View>
       </View>
       <View style={{ flex: 1 }}>
         <View
@@ -67,7 +145,9 @@ export default function BackupScreen({ navigation }) {
         >
           <View
             style={{
+              // flex: 1,
               alignSelf: 'stretch',
+
               paddingHorizontal: 16,
               paddingVertical: 8,
               ...globalStyles.flex.row,
@@ -106,18 +186,74 @@ export default function BackupScreen({ navigation }) {
 
             <TouchableOpacity
               style={{ ...globalStyles.flex.center }}
-              onPress={handleBackup}
+            // onPress={handleBackup}
             >
-              <AppText
-                bold
-                color="secondaryColor"
-                style={{ paddingVertical: 12 }}
+              {/* <AppText
+								bold
+								color="secondaryColor"
+								style={{ paddingVertical: 12 }}
+							>
+								Backup
+							</AppText> */}
+              <Pressable
+                onPress={() => {
+                  navigate(routes.itemPicker, {
+                    items: [
+                      { id: 1, title: 'Local Phone' },
+                      { id: 2, title: 'Google Drive' },
+                    ],
+                    onSelect: (item) => {
+                      setBackupPlan(item)
+                    }
+                  })
+                }}
+
+              // style={{
+              // 	backgroundColor: globalStyles.Colors.inputColor,
+              // 	borderRadius: 10,
+              // 	height: 55,
+              // 	alignItems: 'center',
+              // 	justifyContent: 'space-between',
+              // 	alignSelf: 'stretch',
+              // 	flexDirection: 'row',
+              // 	paddingHorizontal: 16,
+              // }}
               >
-                Backup
-              </AppText>
+                <View style={{ flexDirection: 'row' }}>
+                  {/* <FontAwesome5Icon style={{ marginLeft: 4 }} size={15} color={globalStyles.Colors.text2} name="map-marker-alt" /> */}
+                  <AppText bold
+                    color="secondaryColor"
+                    style={{ paddingVertical: 12 }}>Backup</AppText>
+                </View>
+                {/* <AppIcon name="arrowRightCircle" /> */}
+              </Pressable>
             </TouchableOpacity>
+
+
           </View>
         </View>
+
+        {backup && backupPlan?.id === 1 ? handleBackup() : <></>}
+
+        {backup && backupPlan?.id === 2 ? <View style={{ ...globalStyles.flex.center }}>
+          <AppText
+            style={{
+              textAlign: 'center',
+              paddingHorizontal: 36,
+              paddingVertical: 8,
+            }}
+            typo="tiny"
+          >
+            Upload your Backup on your Google Drive.
+          </AppText>
+          <GoogleSigninButton
+            onPress={signIn}
+            style={{ width: 192, height: 48 }}
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+          />
+        </View> : <></>}
+
         <View style={{ ...globalStyles.flex.center, marginVertical: 24 }}>
           <AppText
             style={{
@@ -135,11 +271,6 @@ export default function BackupScreen({ navigation }) {
           </AppText>
         </View>
       </View>
-      {/* <AppButton
-        title="Create Wallet"
-        style={{ fontWeight: 'bold' }}
-        onPress={handleAddWallet}
-      /> */}
     </Screen>
   )
 }
