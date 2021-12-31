694594f211239ea17d89ca8689c2b59bf3f683fd
danial <dev.salahi@gmail.com>
2021-12-31 18:07:56 +0300

    added hook for historical price

---
+++ src/hooks/useHistoricalPrice.js
@@ -0,0 +1,40 @@
+import { useQuery } from 'react-query';
+import HttpService from './../services/HttpService'
+
+const fetchHistoricalPrice = async () => {
+
+  return new Promise((resovle, reject) => {
+
+    const data = {
+      "uniqueId": "abc",
+      "action": "historicalPrice",
+      "data": {
+        "symbol": `BTCUSDT`,
+        "timeframe": '4h',
+        "limit": 6
+      }
+    }
+
+    new HttpService("", data).Post(xhr_response => {
+      if (xhr_response) {
+
+
+        const response = xhr_response.data.rates.map(item => {
+          return {
+            date: moment(item.key),
+            value: item.value
+          }
+        });
+        resovle(response);
+
+      }
+    }, err => {
+      console.log('error in use historical price', err);
+      reject(err)
+    })
+  })
+
+};
+
+const useHistoricalPrice = () => useQuery('historical_prices', fetchHistoricalPrice);
+export default useHistoricalPrice;
