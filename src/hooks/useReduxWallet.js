f6d613059e1148532de374a2981655a35a94db3b
Dan Salahi <dan.salahii@gmail.com>
2022-04-27 09:48:32 +0300

    update functions to get balances

--- src/hooks/useReduxWallet.js
+++ src/hooks/useReduxWallet.js
@@ -3,35 +3,42 @@ import bitcoinManager from '../blockchains/BitcoinManager'
 import bscManager from '../blockchains/BscManager'
 import ethManager from '../blockchains/EthManager'
 
-export const useReduxWallet = async (coin) => {
-  if (!coin)
-    return {}
+export const useReduxWallet = async coin => {
+	if (!coin) return {}
 
-  const getStoredMnemonic = async () => {
-    const persist = await AsyncStorage.getItem('persist:root')
-    if (persist !== null) {
-      let item = JSON.parse(persist)
-      if (item !== null) {
-        let wallets = JSON.parse(item["wallets"])
-        return wallets.data ? wallets.data[0] : null
-      }
-    }
-  }
+	const getStoredMnemonic = async () => {
+		const persist = await AsyncStorage.getItem('persist:root')
+		if (persist !== null) {
+			let item = JSON.parse(persist)
+			if (item !== null) {
+				let wallets = JSON.parse(item['wallets'])
+				return wallets.data ? wallets.data[0] : null
+			}
+		}
+	}
 
-  const mnemonic = await getStoredMnemonic()
+	const mnemonic = await getStoredMnemonic()
 
-  try {
-    let balance = 0
-    const coinManager = { ETH: ethManager, BNB: bscManager, BTC: bitcoinManager }
-    let selectedCoin = coinManager[coin.symbol];
-    // const wallet = await selectedCoin.getWalletFromMnemonic(mnemonic?.backup)
-    balance = await selectedCoin.getBalance(coin.address, false);
-    if (!balance)
-      return 0;
-    console.log('REDUX ----------Balance of Wallet: ---->', coin.symbol, balance);
-    return balance
-  } catch (err) {
-    console.log('useReduxWallet', err)
-  }
+	try {
+		let balance = 0
+		const coinManager = {
+			ETH: ethManager,
+			BNB: bscManager,
+			BTC: bitcoinManager,
+		}
+		let selectedCoin = coinManager[coin.symbol]
+		// const wallet = await selectedCoin.getWalletFromMnemonic(mnemonic?.backup)
+		balance = await selectedCoin.getBalance(coin.address, false)
+		if (!balance) return 0
+		if (balance == undefined) return 0
 
+		console.log(
+			'REDUX ----------Balance of Wallet: ---->',
+			coin.symbol,
+			balance
+		)
+		return balance
+	} catch (err) {
+		console.log('useReduxWallet', err)
+	}
 }
--- src/utils/WalletFunctions.js
+++ src/utils/WalletFunctions.js
@@ -4,62 +4,74 @@ import bscManager from '../blockchains/BscManager'
 import ethManager from '../blockchains/EthManager'
 import { persist_root } from './../config/async-storage.json'
 
-
 /**
  * Returns a promise that resolves to the value of the stored mnemonic
- * @return {Promise} 
+ * @return {Promise}
  */
 export const getStoredMnemonic = async () => {
-  const persist = await AsyncStorage.getItem(persist_root)
-  if (persist !== null) {
-    let item = JSON.parse(persist)
-    if (item !== null) {
-      let wallets = JSON.parse(item["wallets"])
-      return wallets.data ? wallets.data[0] : null
-    }
-  }
+	const persist = await AsyncStorage.getItem(persist_root)
+	if (persist !== null) {
+		let item = JSON.parse(persist)
+		if (item !== null) {
+			let wallets = JSON.parse(item['wallets'])
+			return wallets.data ? wallets.data[0] : null
+		}
+	}
 }
-
 
 /**
  * Returns a boolean promise that resolves to the value of the existing wallet
- * @return {Promise} 
+ * @return {Promise}
  */
 export const checkExistsWallet = async () => {
-  const persist = await AsyncStorage.getItem(persist_root)
-  if (persist) {
-    let persistObj = JSON.parse(persist)
-    const wallets = JSON.parse(persistObj["wallets"])
-    if (wallets.data && wallets["data"].length > 0) {
-      return true
-    } else {
-      return false
-    }
-  }
+	const persist = await AsyncStorage.getItem(persist_root)
+	if (persist) {
+		let persistObj = JSON.parse(persist)
+		const wallets = JSON.parse(persistObj['wallets'])
+		if (wallets.data && wallets['data'].length > 0) {
+			return true
+		} else {
+			return false
+		}
+	}
 }
-
 
 /**
  * Returns a decimal promise that value of the coin
- * @return {Promise} 
+ * @return {Promise}
  */
-export const getCoinBalance = async (coin) => {
-  if (!coin)
-    return 0
+export const getCoinBalance = async coin => {
+	if (!coin) return 0
 
-  if (checkExistsWallet()) {
-    try {
-      let balance = 0
-      const coinManager = { ETH: ethManager, BNB: bscManager, BTC: bitcoinManager }
-      let selectedCoin = coinManager[coin.symbol];
-      balance = await selectedCoin.getBalance(coin.address, false);
-      if (!balance)
-        return 0;
+	if (checkExistsWallet()) {
+		try {
+			let balance = 0
+			const coinManager = {
+				ETH: ethManager,
+				BNB: bscManager,
+				BTC: bitcoinManager,
+			}
+			let selectedCoin = coinManager[coin.symbol]
+			balance = await selectedCoin.getBalance(coin.address, false)
 
-      return balance
-    } catch (err) {
-      console.log('Wallet Function get Coin Balance from Network ', err)
-    }
-  } else
-    return 0
+			if (!balance) return 0
+			if (balance == undefined) return 0
+
+			return balance
+		} catch (err) {
+			console.log('Wallet Function get Coin Balance from Network ', err)
+		}
+	} else return 0
+}
+
+export const getCoinBalanceFromNetwork = coin => {
+	let balance = 0
+	// getCoinBalance(coin)
+	// 	.then(res => {
+	// 		balance = res
+	// 	})
+	// 	.catch(err => {
+	// 		console.log('getCoinBalanceFromNetwork', err)
+	// 	})
+	return balance
 }
